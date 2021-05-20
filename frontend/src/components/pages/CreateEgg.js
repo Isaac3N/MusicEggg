3d422d438cd9699595c394ba8f64f7a66666c4de
Isaac3N <theisaac3n@gmail.com>
2021-05-20 13:35:45 +0100

    beta drop down menu

--- frontend/src/components/pages/CreateEgg.js
+++ frontend/src/components/pages/CreateEgg.js
@@ -8,23 +8,43 @@ import '../../components/toggle.scss'
 import TextField from "@material-ui/core/TextField";
 import Checkbox from '@material-ui/core/Checkbox'
 import { makeStyles } from '@material-ui/core/styles';
+import { FormControl } from '@material-ui/core';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const CreateEgg = () => {
-    //text field with material ui
+    //material ui styling 
     const useStyles = makeStyles((theme) => ({
         root: {
           '& > *': {
             margin: theme.spacing(1),
             width: '25ch',
           },
+        }, // texfield styling
+        formControl: {
+            margin: theme.spacing(1),
+            minWidth:120,
         },
+        selectEmpty: {
+            marginTop: theme.spacing(2), 
+        }, 
       }));
     const classes = useStyles()
 
     //checkbox with material ui
     const [checked, setChecked] = useState(false)
+
+    //streaming service dropdown menu
+    const[service, setService] = useState('spotify')
+
+
+    //event handlers
     const handleGuestCanPause = (event)=>{
         setChecked(event.target.checked)
+    }
+    const handleStreamingService = (event)=> {
+        setService(event.target.value)
     }
 
     return (
@@ -53,7 +73,20 @@ const CreateEgg = () => {
             
                         </li>
                         <li className="createRoomList">Choose a Streaming service:
-                            
+                        <FormControl className={classes.formControl}>
+                            <InputLabel id="demo-simple-select-label">on {service}</InputLabel>
+                            <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={service}
+                            onChange={handleStreamingService}
+                            >
+                            <MenuItem value='spotify'>Spotify</MenuItem>
+                            <MenuItem value='Apple Music'>Apple Music</MenuItem>
+                            <MenuItem value='SoundCloud'>SoundCloud</MenuItem>
+                            <MenuItem value='deezer'>deezer</MenuItem>
+                            </Select>
+                      </FormControl>
                         </li>
                     </ul>
                     <div className="btn btn-one">
