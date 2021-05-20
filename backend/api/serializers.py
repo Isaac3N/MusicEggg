7ad8937d656bc18b5c4e989c98a2028994344880
Isaac3N <theisaac3n@gmail.com>
2021-05-20 10:47:55 +0100

    created at field added

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,16 +1,6 @@
-from rest_framework import serializers
-from .models import Room
-
 #serializers allows complex data such as querysets and model instances to be converted to native python datatypes t
 #that can the be easily be rendered into json data 
 
-class RoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Room
-        fields = ('id', 'code', 'host', 'guest_can_pause',
-                  'votes_to_skip', 'created_at')
-
-  
 from rest_framework import serializers
 from .models import Room
 
@@ -25,4 +15,4 @@ class RoomSerializer(serializers.ModelSerializer):
 class CreateRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ('guest_can_pause', 'votes_to_skip', "name_of_room")
+        fields = ('guest_can_pause', 'name_of_room', "name_of_room", "created_at")
