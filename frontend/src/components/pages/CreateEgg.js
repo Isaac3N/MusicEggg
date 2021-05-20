9938862ae61c9b30f49c6b76d32cd4b4f96fe08f
Isaac3N <theisaac3n@gmail.com>
2021-05-20 12:43:08 +0100

    beta text input field
    beta text input field using material ui

--- frontend/src/components/pages/CreateEgg.js
+++ frontend/src/components/pages/CreateEgg.js
@@ -5,8 +5,19 @@ import Contact from '../Contact.js'
 import '../../components/button.css'
 import RadioButton from '../../components/RadioButton'
 import '../../components/toggle.scss'
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from '@material-ui/core/styles';
 
 const CreateEgg = () => {
+    const useStyles = makeStyles((theme) => ({
+        root: {
+          '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+          },
+        },
+      }));
+      const classes = useStyles()
     return (
         <div className='hero-container'>
             <Link to=''>
@@ -18,7 +29,11 @@ const CreateEgg = () => {
                     <p>Create a room to share your music with friends.</p> 
                 
                     <ul className='options'>
-                        <li className="roomName">Give your room a name so people would know what room they are joining </li>
+                        <form className = {classes.root} noValidate autoComplete='off'>
+                            <li className="roomName">Give your room a name so people would know what room they are joining 
+                                <TextField id="standard-basic" label="Strange monkey &#58664;" />   
+                            </li>
+                        </form>
                         <li className="createRoomList">Guest Can have control to play/pause songs?
                             <label class="switch-wrap">
                             <input type="checkbox" />
@@ -26,24 +41,7 @@ const CreateEgg = () => {
                             </label> 
                         </li>
                         <li className="createRoomList">Choose a Streaming service:
-                            <ul>
-                               <RadioButton
-                                    id='1'
-                                    label='spotify'
-                                />
-                                <RadioButton
-                                    id='2'
-                                    label='Deezer'
-                                />
-                                <RadioButton
-                                    id='3'
-                                    label='SoundCloud'
-                                />
-                                <RadioButton
-                                    id='4'
-                                    label='Apple Music'
-                                />
-                            </ul>
+                            
                         </li>
                     </ul>
                     <div class="btn btn-one">
