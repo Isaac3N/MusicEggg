af3a12f0960974c9fe41eb3288a6ee82e100ff3d
Isaac3N <theisaac3n@gmail.com>
2021-05-22 14:21:27 +0100

    join egg page created

--- frontend/src/components/pages/JoinEgg.js
+++ frontend/src/components/pages/JoinEgg.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import './JoinEgg.css'
+import { Link } from 'react-router-dom';
+import TextField from "@material-ui/core/TextField";
 
 const JoinEgg = () => {
     return (
-        <div>
-            <h1>This is the the join Egg page</h1>
+        <div className='hero-container'>
+            
+            <Link to=''>
+                <h3 className ='top-left'>Music Egg</h3>
+            </Link>
+            <div class = 'container'>
+                <div className ='joinRoom'>
+                    <h4>Join an Egg </h4>
+                    <TextField id="outlined-search"
+                        label="Join Egg" 
+                        type="search" 
+                        variant="outlined" />
+                    <div className="btn btn-one">
+                        <span>Join Egg</span>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     )
 }
