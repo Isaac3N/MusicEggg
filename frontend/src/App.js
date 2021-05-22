1dc07df68da343f6664d2c576453dabf2bf5d137
Isaac3N <theisaac3n@gmail.com>
2021-05-22 20:25:03 +0100

    room sessions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,33 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
 import Home from './components/pages/Home'
 import CreateEgg from './components/pages/CreateEgg'
 import Egg from './components/pages/Egg';
 import JoinEgg from './components/pages/JoinEgg';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const[code, setCode] = useState(null)
+  useEffect(()=>{
+    async function fetchData(){
+      fetch('http://localhost:8000/api/user-in-room')
+      .then((response)=> response.json())
+      .then((data)=>{
+        setCode({
+          code: data.code, 
+        })
+      })
+    }
+    fetchData()
+  }, [])
   return (
     <>
       <Router>
         <Switch>
-          <Route path='/' exact component={Home} />
+          <Route path='/' exact component={Home} render={()=>{
+            return code ? (<Redirect to={`/egg/${code}`}/>) : (Home())
+          }}/>
           <Route path='/CreateEgg' component={CreateEgg}/>
           <Route path='/JoinEgg' component={JoinEgg}/>
           <Route path='/egg/:eggCode' component={Egg}/>
--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../App.css';
 import Cards from '../Cards';
 import Contact from '../Contact';
 import HeroSection from '../HeroSection';
 import PoweredBy from '../PoweredBy';
+import axios from 'axios'
+
 
 function Home() {
+  
+
   return (
     <>
      <HeroSection/>
