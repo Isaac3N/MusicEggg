f316f20ec0ef2f2b477a812a1ef691506f449d71
Isaac3N <theisaac3n@gmail.com>
2021-05-22 02:32:42 +0100

    api views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from .serializers import RoomSerializer, CreateRoomSerializer
+from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None):
+        #sessions is a temporary connetcion between two computers 
+        if not self.request.session.exists(self.request.session.session_key): # to check whether the session exist
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data) # if the session exist it gets the data
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            name_of_room = serializer.data.get('name_of_room')
+            streaming_service = serializer.data.get('streaming_service')
+            host = self.request.session.session_key
+            queryset = Room.objects.filter(host=host)
+            if queryset.exists(): #to check if teh host has any other rooms 
+                room = queryset[0]
+                room.name_of_room = name_of_room 
+                room.guest_can_pause = guest_can_pause
+                room.streaming_service = streaming_service
+                room.save(update_fields=['guest_can_pause', 'name_of_room', 'created_at', 'streaming_service'])
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+            else:
+                room = Room(host=host, guest_can_pause=guest_can_pause,
+                            name_of_room=name_of_room,  streaming_service=streaming_service)
+                room.save()
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED) #to return a json formatted data 
+
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
