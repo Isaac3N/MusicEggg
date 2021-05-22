9a8be4271b64073b275e251e5e4aa92aea5ca746
Isaac3N <theisaac3n@gmail.com>
2021-05-22 12:14:10 +0100

    egg page created

--- frontend/src/components/pages/Egg.js
+++ frontend/src/components/pages/Egg.js
@@ -6,13 +6,34 @@ const Egg = ({match}) => {
     const[service, setService] = useState('spotify')
     const [ishost, setIshost] = useState(false)
     const eggCode = match.params.eggCode // match stores the information of how we got into the component using router
+    
+    const getEggDetails=()=> {
+        fetch("/api/get-room?code=" + eggCode)
+          .then((response) => response.json())
+          .then((data) => {
+            setTitle(
+              data.name_of_room
+            )
+            setChecked(
+                data.guest_can_pause,
+            )
+            setIshost(
+                data.is_host,
+            )
+            setService(
+                data.streaming_service, 
+            )
+          });
+      }
+      getEggDetails()
+
     return (
         <div>
             <h3>{eggCode}</h3>
             <p>Title: {title}</p>
-            <p>checked: {checked}</p>
+            <p>guest can pause: {checked.toString()}</p>
             <p>Service used: {service}</p>
-            <p>Host: {ishost}</p>
+            <p>Host: {ishost.toString()}</p>
         </div>
     )
 }
