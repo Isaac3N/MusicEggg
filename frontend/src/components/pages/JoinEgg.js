37a0f665912b595a8543fa0a385ad88306feb504
Isaac3N <theisaac3n@gmail.com>
2021-05-22 14:42:45 +0100

    join egg updates

--- frontend/src/components/pages/JoinEgg.js
+++ frontend/src/components/pages/JoinEgg.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './JoinEgg.css'
 import { Link } from 'react-router-dom';
 import TextField from "@material-ui/core/TextField";
 
 const JoinEgg = () => {
+    const [code, setCode] = useState()
+    const handleCodeInput = (event) =>{
+        setCode(event.target.value)
+    }
+    const handleButttonPressed = ()=> {
+        console.log(code) 
+    }
     return (
         <div className='hero-container'>
-            
             <Link to=''>
                 <h3 className ='top-left'>Music Egg</h3>
             </Link>
@@ -14,10 +20,13 @@ const JoinEgg = () => {
                 <div className ='joinRoom'>
                     <h4>Join an Egg </h4>
                     <TextField id="outlined-search"
-                        label="Join Egg" 
+                        value = {code}
+                        onChange = {handleCodeInput}
+                        placeholder = 'Enter an Egg Code'
+                        label="Egg Code" 
                         type="search" 
                         variant="outlined" />
-                    <div className="btn btn-one">
+                    <div className="btn btn-one" onClick={handleButttonPressed } >
                         <span>Join Egg</span>
                     </div>
                 </div>
