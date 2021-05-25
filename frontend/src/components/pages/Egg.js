dc9c74a81dea6e16bd91b65d216f8b4e53145e00
Isaac3N <theisaac3n@gmail.com>
2021-05-25 03:30:36 +0100

    fixed redirect bug

--- frontend/src/components/pages/Egg.js
+++ frontend/src/components/pages/Egg.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-import CreateEgg from './CreateEgg';
 
 
 const Egg = (props) => {
@@ -10,20 +9,18 @@ const Egg = (props) => {
     const[service, setService] = useState('spotify')
     const [isHost, setIsHost] = useState(false)
     const[skip, setSkip] = useState(2)
-    const[code, setCode] = useState(null)
-    const[settings, setSettings] = useState(false)
-    
     const eggCode = props.match.params.eggCode // match stores the information of how we got into the component using router
+    const[code, setCode] = useState(null)
+
     const clearRoomCode=()=>{
       setCode({code: null})
     }
-    
 
     const getEggDetails=()=> {
         fetch("/api/get-room" + "?code=" + eggCode)
           .then((response) => {
             if (!response.ok) {
-              clearRoomCode()
+              clearRoomCode();
               props.history.push("/");
             }
             return response.json();
@@ -56,60 +53,24 @@ const Egg = (props) => {
         };
         fetch("/api/leave-room", requestOptions)
         .then((_response) => {
-          clearRoomCode()
+          clearRoomCode();
           props.history.push("/");
         });
       }
-      const updateSettings=(value)=>{
-        setSettings(
-          {
-            settings: value, 
-          }
-        )
-      }
-      const renderSettings=()=>{
-        return (
-          <div>
-            <CreateEgg
-              update={true}
-              votesToSkip = {skip}
-              guestCanPause = {checked}
-              code = {code}
-              updateCallBack= {null}
-            />
-            <Button variant="contained" color="secondary" onClick={()=> updateSettings(false)}>
-                    Close
+
+    return (
+        <div>
+            <h3>{eggCode}</h3>
+            <p>Title: {title}</p>
+            <p>guest can pause: {checked.toString()}</p>
+            <p>Votes Required to skip: {skip}</p>
+            <p>Service used: {service}</p>
+            <p>Host: {isHost.toString()}</p>
+            <Button variant="contained" color="secondary" to="/" component={Link} onClick={leaveButtonPressed}>
+                Leave Room 
             </Button>
-          </div>
-          )
-      }
-
-      const showSettingsButton=()=>{
-        return(
-          <Button variant="contained" color="primary" onClick={()=> updateSettings(true)}>
-                Settings
-          </Button>
-        )
-      }
-
-    if (settings){
-      return renderSettings()
-    }
-    else {
-      return (
-          <div>
-              <h3>{eggCode}</h3>
-              <p>Title: {title}</p>
-              <p>guest can pause: {checked.toString()}</p>
-              <p>Votes Required to skip: {skip}</p>
-              <p>Service used: {service}</p>
-              <p>Host: {isHost.toString()}</p>
-              {isHost? showSettingsButton(): null}
-              <Button variant="contained" color="secondary" to="/" component={Link} onClick={leaveButtonPressed}>
-                  Leave Room 
-              </Button>
-          </div>
-      )}
+        </div>
+    )
 }
 
 export default Egg
